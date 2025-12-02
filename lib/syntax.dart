5b0304f8c0aca1960786d7f11605c2abf826cced
Shang <shang1219178163@gmail.com>
2025-12-02 10:44:44 +0800

    fix: bug

--- lib/syntax.dart
+++ lib/syntax.dart
@@ -139,7 +139,7 @@ class TypeDefinition {
         }
       }
 
-      return "if (json['$key'] != null) {\n\t\t\tfinal array = List<Map<String, dynamic>>.from(json['packages'] ?? []);\n\t\t\t$fieldKey = array.map((e) => ProjectPackageModel.fromJson(e)).toList();\n\t\t}";
+      return "if (json['$key'] != null) {\n\t\t\tfinal array = List<Map<String, dynamic>>.from(json['$key'] ?? []);\n\t\t\t$fieldKey = array.map((e) => $subtype.fromJson(e)).toList();\n\t\t}";
       return "if (json['$key'] != null) {\n\t\t\tfinal array = (json['$key'] as List<Map<String, dynamic>>).map((e) => $subtype.fromJson(e));\n\t\t\t$fieldKey = List<$subtype>.from(array);\n\t\t}";
     } else {
       // class
